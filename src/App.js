b04d897b69eb103fce723facb37edfa82111659e
jihoonshin <jip68660@gmail.com>
2019-08-20 20:16:30 -0500

    save datas after user input the text that wanted to be listed

--- src/App.js
+++ src/App.js
@@ -4,10 +4,51 @@ import UserInput from "./component/UserInput";
 import TodoListItem from "./component/TodoListItem";
 
 class App extends Component {
+  id = 0;
+
+  state = {
+    input: "",
+    todo: []
+  }
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    });
+  }
+  handleCreate = (e) => {
+    const {input, todo} = this.state;
+    this.setState({
+      input: "",
+      todo: todo.concat({
+        id: this.id++,
+        text: input,
+        status: false
+      })
+    });
+  }
+  handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      this.handleCreate(e);
+    }
+  }
+
   render() {
+    const { input } = this.state;
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this;
+
     return (
       <div>
-        <TodoListFormat form={<UserInput/>}>
+        <TodoListFormat form={
+          <UserInput 
+            value={input}
+            onKeyPress={handleKeyPress}
+            onChange={handleChange}
+            onCreate={handleCreate} />
+        }>
           <TodoListItem/>
         </TodoListFormat>
       </div>
